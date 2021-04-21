47ad39280c22251d6bdc688e91238fa0cea4194b
natalie <natalie.itzhak@gmail.com>
2021-04-22 02:02:17 +0300

    20.1

--- controllers/products.contorller.js
+++ controllers/products.contorller.js
@@ -14,64 +14,110 @@ const createProduct = async (req, res) => {
     try {
         const result = await product.save();
         return res.status(201).json({ "success": result });
-      } catch (error) {
+    } catch (error) {
         return res.status(500).json({ "error": error })
-      }
+    }
+}
+
+const getProductbyName = async (req, res) => {
+    try {
+        const { name } = req.params;
+        const product = await productModel.findOne({ name: name });
+        if (!product) {
+            return res.status(404).json({ error: "Product not found" });
+        }
+        return res.send(product);
+    } catch (error) {
+        return res.status(500).json({ "error": error })
     }
 
-      const getProductbyName = async (req, res) => {
-        try {
-          const { name } = req.params;
-          const product = await productModel.findOne({ name: name });
-          if (!product) {
-            return res.status(404).json({ error: "Product not found" });
-          }
-          return res.send(product);
-        } catch (error) {
-          return res.status(500).json({ "error": error })
-        }
-      
-    }
-     
+}
+
 const getProduct = async (req, res) => {
     const { id } = req.params;
     try {
-      const product = await productModel.findById(id);
-      if (!product) {
-        return res.status(404).send("Wrong ID");
-      }
-      res.json(product);
+        const product = await productModel.findById(id);
+        if (!product) {
+            return res.status(404).send("Wrong ID");
+        }
+        res.json(product);
     } catch (error) {
-      res.status(500).send(error);
+        res.status(500).send(error);
     }
-  };
-  
-  const getActiveProducts = async (req, res) => {
-    try {
-      const products = await productModel.find({ isActive: true });
-      res.send(products);
-    } catch (error) {
-      res.status(500).send(error);
-    }
-  };
-  
-  const getProductsByPriceRange = async (req, res) => {
-    const { min, max } = req.body;
-    console.log(req.body)
-    try {
-      const products = await productModel.find({
-        "details.price": { $gte: min, $lte: max },
-      });
-      res.send(products);
-    } catch (error) {
-      res.status(500).send(error);
-    }
-  };
-module.exports = {
-    create: createProduct,
-    getAll: getProduct,
-    getProductbyName,
-    getActiveProducts,
-    getProductsByPriceRange
+};
 
+const getActiveProducts = async (req, res) => {
+    try {
+        const products = await productModel.find({ isActive: true });
+        res.send(products);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+};
+
+const getProductsByPriceRange = async (req, res) => {
+    const { min, max } = req.body;
+    try {
+        const products = await productModel.find({
+            "details.price": { $gte: min, $lte: max },
+        });
+        res.send(products);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+    const update = async (req, res) => {
+        const updates = Object.keys(req.body);
+        const allowedUpdates = ["isActive", "details.discount"];
+        const isValidOperation = updates.every((update) =>
+            allowedUpdates.includes(update)
+        );
+        if (!isValidOperation) return res.status(400).send("Invalid update");
+
+        try {
+            const product = await productModel.findByIdAndUpdate(
+                req.params.id,
+                req.body,
+                { new: true, runValidators: true }
+            );
+            if (!product) {
+                return res.status(404).send("This ID was not found");
+            }
+            res.send(product);
+        } catch (error) {
+            res.status(400).send(error);
+        }
+    };
+
+    const deleteProduct = async (req, res) => {
+        try {
+            const product = await productModel.findByIdAndDelete(req.params.id);
+            if (!product) {
+                return res.status(404).send("This ID was not found");
+            }
+            res.send(product);
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    };
+
+    const deleteAllProducts = async (req, res) => {
+        try {
+            const response = await productModel.deleteMany();
+            res.send(response);
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    };
+
+    module.exports = {
+        create: createProduct,
+        getAll: getProduct,
+        getProductbyName,
+        getActiveProducts,
+        getProductsByPriceRange,
+        update,
+        deleteProduct,
+        deleteAllProducts
+
+    }
 }
--- routes/products.route.js
+++ routes/products.route.js
@@ -4,18 +4,26 @@ const productsControler = require('../controllers/products.contorller');
 
 
 router
-.get('/', (req, res) => {
-   productControler.getProducts(req, res);
- })
- .get('/active', (req, res) => {
-   productControler.activeProducts(req, res);
- })
- .get('/price', (req, res) => {
-   productControler.getByPriceRange(req, res);
- }).get('/',(req,res)=>{
-   productsControler.getAll(req,res);
-}).post('/',(req,res)=>{
-   productsControler.create(req,res);
-})
+   .get('/', (req, res) => {
+      productControler.getProducts(req, res);
+   })
+   .get('/active', (req, res) => {
+      productControler.activeProducts(req, res);
+   })
+   .get('/price', (req, res) => {
+      productControler.getByPriceRange(req, res);
+   }).get('/', (req, res) => {
+      productsControler.getAll(req, res);
+   }).post('/', (req, res) => {
+      productsControler.create(req, res);
+   }).put('/update/:id', (req, res) => {
+      productsControler.update(req, res);
+   }).delete('/:id', (req, res) => {
+      productsControler.deleteProduct(req, res);
+   }).delete('/', (req, res) => {
+      productsControler.deleteAllProducts(req, res);
+   })
+
+
 
 module.exports = router;
